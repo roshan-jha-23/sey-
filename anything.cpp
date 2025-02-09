5c270822d0839b0fef6d9294f25b9d5f30b7c1c4
roshan-jha-23 <jha062305@gmail.com>
2025-02-10 00:07:27 +0530

    my first proj

---
+++ anything.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int MOD = 1e9 + 7;
+const int oo = 1e9;
+const ll OO = 1e18;
+
+#define on 1
+#define off 0
+
+void solve()
+{
+    cout << "haha";
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    t = 1;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+// if you
+// make yourself more than just a man, if you devote yourself
+// to an ideal, and if they can't stop you, then you become
+// something else entirely.
+// --------------------------A Legend-------------------------
